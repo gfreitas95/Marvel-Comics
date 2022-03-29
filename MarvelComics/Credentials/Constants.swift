7e68d969f00edad853b00e0921e31ebaf1e99ba2
gfreitas95 <heygu95@gmail.com>
2022-03-28 23:48:09 -0300

    Setup Project.

--- MarvelComics/Credentials/Constants.swift
+++ MarvelComics/Credentials/Constants.swift
@@ -10,5 +10,5 @@ import UIKit
 struct Constants {
     
     static let PUBLIC_KEY = "ee990fa1f09f21290fb76b8d9a00afb1"
-    static let PRIVATE_KEY = "[YOUR PRIVATE KEY GOES HERE]"
+    static let PRIVATE_KEY = "[YOUR PRIVATE KEY GOES HERE 😁]"
 }
